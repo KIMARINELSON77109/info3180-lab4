45827d370ede3f382cdbe717fc08d637649a2606
KIMARI NELSON <kimari.nelson@mymona.uwi.edu>
2018-02-22 22:45:56 +0000

    EXERCISE 1: File Upload

--- app/views.py
+++ app/views.py
@@ -5,9 +5,10 @@ Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
 import os
-from app import app
+from app import app, UploadFolder
 from flask import render_template, request, redirect, url_for, flash, session, abort
 from werkzeug.utils import secure_filename
+from form import UploadForm
 
 
 ###
@@ -30,17 +31,19 @@ def about():
 def upload():
     if not session.get('logged_in'):
         abort(401)
-
     # Instantiate your form class
-
+    U_form = UploadForm()
     # Validate file upload on submit
     if request.method == 'POST':
         # Get file data and save to your uploads folder
+        if U_form.validate_on_submit():
+            File = U_form.upload.data
+            Filename = secure_filename(File.filename)
+            File.save(os.path.join(UploadFolder, Filename))
+            flash('File Saved', 'success')
+            return redirect(url_for('home'))
 
-        flash('File Saved', 'success')
-        return redirect(url_for('home'))
-
-    return render_template('upload.html')
+    return render_template('upload.html',  form = U_form)
 
 
 @app.route('/login', methods=['POST', 'GET'])
